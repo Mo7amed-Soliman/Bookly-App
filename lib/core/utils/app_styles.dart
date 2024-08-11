991b00c2598556ad5cf3a02bdd2f4e2d6070d981
Mohamed Soliman <ms3196@fayoum.edu.eg>
2024-08-11 03:27:43 +0300

    add app styles

---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,32 @@
+import 'package:bookly_app/core/utils/constants.dart';
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static const TextStyle textW600Style18 = TextStyle(
+        fontSize: 18,
+        color: Colors.white,
+        fontWeight: FontWeight.w600,
+      ),
+      textW500Style16 = TextStyle(
+        fontSize: 16,
+        color: Colors.white,
+        fontWeight: FontWeight.w500,
+      ),
+      textStyle14 = TextStyle(
+        fontSize: 14,
+        color: Colors.white,
+        fontWeight: FontWeight.normal,
+      ),
+      textStyle20 = TextStyle(
+        fontSize: 20,
+        color: Colors.white,
+        fontFamily: kGTSectraFine,
+        fontWeight: FontWeight.normal,
+      ),
+      textStyle30 = TextStyle(
+        fontSize: 30,
+        color: Colors.white,
+        fontFamily: kGTSectraFine,
+        fontWeight: FontWeight.normal,
+      );
+}
