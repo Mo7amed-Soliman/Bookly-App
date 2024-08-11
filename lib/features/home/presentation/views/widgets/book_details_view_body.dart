2a239cd94a848e43d7d709a02cc91c9d6133fecd
Mohamed Soliman <ms3196@fayoum.edu.eg>
2024-08-12 02:40:12 +0300

    add book title and author

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,5 +1,7 @@
+import 'package:bookly_app/core/utils/app_styles.dart';
 import 'package:bookly_app/core/utils/constants.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -7,12 +9,33 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: defaultPadding),
+    double width = MediaQuery.sizeOf(context).width;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
       child: Column(
         children: [
-          SizedBox(height: 12),
-          CustomBookDetailsAppBar(),
+          const SizedBox(height: 12),
+          const CustomBookDetailsAppBar(),
+          const SizedBox(height: 30),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: width * 0.17,
+            ),
+            child: const CustomBookImage(),
+          ),
+          const SizedBox(height: 43),
+          const Text(
+            'The Jungle Book',
+            style: AppStyles.textStyle30,
+          ),
+          const SizedBox(height: 6),
+          Text(
+            'J. K. Rowling',
+            style: AppStyles.textW600Style18.copyWith(
+              fontStyle: FontStyle.italic,
+              color: Colors.grey,
+            ),
+          ),
         ],
       ),
     );
