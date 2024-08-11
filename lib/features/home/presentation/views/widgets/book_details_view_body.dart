4c3eca7f7889b69f7a221fb0042e3e70a4c0d2b8
Mohamed Soliman <ms3196@fayoum.edu.eg>
2024-08-12 02:48:56 +0300

    add book rating

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/core/utils/app_styles.dart';
 import 'package:bookly_app/core/utils/constants.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
@@ -24,6 +25,7 @@ class BookDetailsViewBody extends StatelessWidget {
             child: const CustomBookImage(),
           ),
           const SizedBox(height: 43),
+          //? book title and author
           const Text(
             'The Jungle Book',
             style: AppStyles.textStyle30,
@@ -35,6 +37,11 @@ class BookDetailsViewBody extends StatelessWidget {
               fontStyle: FontStyle.italic,
               color: Colors.grey,
             ),
+          ),
+          const SizedBox(height: 16),
+          //? book rating
+          const BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
           ),
         ],
       ),
--- lib/features/home/presentation/views/widgets/book_rating.dart
+++ lib/features/home/presentation/views/widgets/book_rating.dart
@@ -3,25 +3,27 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
-
+  const BookRating(
+      {super.key, this.mainAxisAlignment = MainAxisAlignment.center});
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      children: [
+    return Row(
+      mainAxisAlignment: mainAxisAlignment,
+      children: const [
         Icon(
           FontAwesomeIcons.solidStar,
           color: Colors.yellow,
-          size: 18.0,
+          size: 22.0,
         ),
-        SizedBox(width: 6.0),
+        SizedBox(width: 8.0),
         Text(
           '4.8',
           style: AppStyles.textW500Style16,
         ),
         SizedBox(width: 3.0),
         Text(
-          '(2300)',
+          '(2450)',
           style: AppStyles.textStyle14,
         ),
       ],
