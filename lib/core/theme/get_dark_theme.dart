9d0324d905a7fa05ab18fbb4f6e20196dcb5f18d
Mohamed Soliman <ms3196@fayoum.edu.eg>
2024-08-11 03:27:06 +0300

    add fontFamily

--- lib/core/theme/get_dark_theme.dart
+++ lib/core/theme/get_dark_theme.dart
@@ -1,10 +1,14 @@
 import 'package:bookly_app/core/utils/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 ThemeData getDarkTheme() {
   return ThemeData(
     useMaterial3: true,
     brightness: Brightness.dark,
     scaffoldBackgroundColor: kBackgroundColor,
+    textTheme: GoogleFonts.montserratTextTheme(
+      ThemeData.dark().textTheme,
+    ),
   );
 }
