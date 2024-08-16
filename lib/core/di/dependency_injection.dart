745421832502f74f91eaead40cb0c17bdb97ed94
Mohamed Soliman <ms3196@fayoum.edu.eg>
2024-08-16 05:35:38 +0300

    add dependency injection (get it)

---
+++ lib/core/di/dependency_injection.dart
@@ -0,0 +1,14 @@
+import 'package:bookly_app/core/networking/api_service.dart';
+import 'package:bookly_app/features/home/data/repo/home_repo_impl.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+void setupGetIt() {
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  getIt.registerSingleton<HomeRepoImpl>(
+    HomeRepoImpl(
+      getIt.get<ApiService>(),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:bookly_app/app/bookly_app.dart';
+import 'package:bookly_app/core/di/dependency_injection.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  setupGetIt();
   runApp(const BooklyApp());
 }
