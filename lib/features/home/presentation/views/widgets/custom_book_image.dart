9bda969a3a1375b93a0394587277c9267cd2a7a3
Mohamed Soliman <ms3196@fayoum.edu.eg>
2024-08-18 01:16:56 +0300

    add cached network image

--- lib/features/home/presentation/views/widgets/custom_book_image.dart
+++ lib/features/home/presentation/views/widgets/custom_book_image.dart
@@ -1,25 +1,35 @@
-import 'package:bookly_app/core/utils/app_assets.dart';
 import 'package:bookly_app/core/utils/constants.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
   const CustomBookImage({
     this.borderRadius = defaultRadius,
     super.key,
+    required this.imageUrl,
   });
   final double borderRadius;
+  final String imageUrl;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(borderRadius),
-          image: const DecorationImage(
-            image: AssetImage(AppAssets.testImage),
-            fit: BoxFit.fill,
-          ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(borderRadius),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: CachedNetworkImage(
+          imageUrl: imageUrl,
+          fit: BoxFit.fill,
+          errorWidget: (context, url, error) => const Icon(Icons.error),
         ),
+        // child: Container(
+        //   decoration: BoxDecoration(
+        //     borderRadius: BorderRadius.circular(borderRadius),
+        //     image: const DecorationImage(
+        //       image: AssetImage(AppAssets.testImage),
+        //       fit: BoxFit.fill,
+        //     ),
+        //   ),
+        // ),
       ),
     );
   }
