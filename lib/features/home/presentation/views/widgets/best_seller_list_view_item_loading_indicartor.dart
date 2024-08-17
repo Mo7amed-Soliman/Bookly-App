876d593a7936292772a8af3cc7e6b85acabb9983
Mohamed Soliman <ms3196@fayoum.edu.eg>
2024-08-18 02:12:11 +0300

    create best seller list view loading indicator

---
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item_loading_indicartor.dart
@@ -0,0 +1,56 @@
+import 'package:bookly_app/core/utils/constants.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image_loading_indicator.dart';
+import 'package:flutter/material.dart';
+
+class BestSellerListViewItemLoadingIndicartor extends StatelessWidget {
+  const BestSellerListViewItemLoadingIndicartor({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: SizedBox(
+        height: 126.0,
+        child: Row(
+          children: [
+            const CustomBookImageLoadingIndicator(),
+            const SizedBox(width: defaultPadding),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 25.0),
+                  Container(
+                    height: 6.0,
+                    color: Colors.white30,
+                  ),
+                  const SizedBox(height: 25.0),
+                  Container(
+                    height: 6.0,
+                    color: Colors.white30,
+                  ),
+                  const SizedBox(height: 30.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        height: 4.0,
+                        width: 60.0,
+                        color: Colors.white30,
+                      ),
+                      Container(
+                        height: 4.0,
+                        width: 85.0,
+                        color: Colors.white30,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_loading_indicator.dart
@@ -0,0 +1,23 @@
+import 'package:bookly_app/core/utils/constants.dart';
+import 'package:bookly_app/core/widgets/custom_fading_widget.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view_item_loading_indicartor.dart';
+import 'package:flutter/material.dart';
+
+class BestSellerListViewLoadingIndicator extends StatelessWidget {
+  const BestSellerListViewLoadingIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomFadingWidget(
+      child: ListView.builder(
+        padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return const BestSellerListViewItemLoadingIndicartor();
+        },
+      ),
+    );
+  }
+}
